3d3a633cf387575046eb0081de9d409b9a194042
Athir Nuaimi <athir@nuaimi.com>
2023-06-21 18:15:26 -0400

    add droplet to DO project

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
 
-
 // slug size - from https://slugs.do-api.dev
 const SLUG_TINY = digitalocean.DropletSlug.DropletS1VCPU1GB;
 const SLUG_BASIC = digitalocean.DropletSlug.DropletS2VCPU4GB_INTEL;
@@ -9,28 +8,37 @@ const SLUG_MEM64 = digitalocean.DropletSlug.DropletM8VCPU64GB;
 
 const VOLUME_SIZE = 100
 
+export = async () => {
 
-const volume = new digitalocean.Volume("mapdisk", {
-  region: digitalocean.Region.TOR1,
-  size: VOLUME_SIZE, 
-  initialFilesystemType: "ext4",
-  description: "map tile data",
-});
+  const mySshKey = await digitalocean.getSshKey({
+    name: "anuaimi@devfoundry.com",
+  });
 
-// get details on default sshkey
-let fingerprint: string;
-const sshPromise = digitalocean.getSshKey({
-  name: "anuaimi@devfoundry.com",
-}).then((sshKey)=>{
-
-  fingerprint = sshKey.fingerprint
-
-  const droplet = new digitalocean.Droplet("buildmaps", {
+  const volume = new digitalocean.Volume("geodata", {
+    region: digitalocean.Region.TOR1,
+    size: VOLUME_SIZE, 
+    initialFilesystemType: "ext4",
+    description: "map tile data",
+  });
+  
+  // create resources
+  const droplet = new digitalocean.Droplet("planetiler", {
     image: "debian-11-x64",
     region: "tor1",
     size: SLUG_TINY,
     volumeIds: [volume.id],
-    sshKeys: [fingerprint],
+    sshKeys: [mySshKey.fingerprint],
+    monitoring: true,
+    ipv6: true,
   });
-  
-});
+
+  // create project for these resources
+  const project = new digitalocean.Project( "map-builder", {
+      resources: [
+        droplet.id.apply(id => `do:droplet:${id}`),
+      ],
+    }
+  );
+
+  return { ip: droplet.ipv4Address };
+}
